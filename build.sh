d0e1c35bbcf057c96e27b2325257cc28d6e9d871
Baptiste Wicht <baptiste.wicht@gmail.com>
2025-05-15 16:11:45 +0200

    Sanity checks

--- build.sh
+++ build.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+# Build the image
 docker build -f Dockerfile -t cpp:latest .
+
+# Sanity checks
+docker run -it cpp:latest g++-14 --version
+docker run -it cpp:latest g++-15 --version
+docker run -it cpp:latest clang++-18 --version
+docker run -it cpp:latest clang++-20 --version
+
+# Release the image
 docker tag cpp:latest wichtounet/cpp:latest
 docker push wichtounet/cpp:latest
